045c18ac8094f122c00899798bf8c148f3ab4657
Hazel Upston <h@zle.au>
2026-05-08 11:04:36 +1000

    Modes, minor changes related

--- theme.sh
+++ theme.sh
@@ -1,20 +1,9 @@
 #!/bin/zsh
 rm -r ~/.cache/wal/
 
-if [ "$1" = "--light" ] 
-then
-    wal -li ~/wallpaper
-else
-    wal -i ~/wallpaper
-fi
-
-sleep 2
+wal -i ~/wallpaper
 
 pkill hyprpaper; pkill waybar
 hyprpaper & waybar & disown
-
-sleep 1; kitty +kitten panel --edge background --lines 15 --config ~/.config/kitty/panel.conf zsh -c "cava -p ~/.config/cava/cava-background" & disown
-
-sleep 1; pkill kanshi && kanshi
 
 hyprctl reload
